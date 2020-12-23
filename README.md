2171d4100070a144f9992452464c3e8a2b145afc
vasilijus <vasiliokas@gmail.com>
2020-12-23 17:14:55 +0000

    project/ Stream add/delete/set

--- README.md
+++ README.md
@@ -5,9 +5,21 @@ package needle for twitter api
 implement socket.io
 
 // Twitter
+https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/quick-start
 https://developer.twitter.com/en/application/use-case
 
 
 # Steps 
 npm init
 npm i express socket.io needle dotenv
+npm i -D node
+
+(Needle - http client)
+package.json
+```
+  "scripts": {
+```
+
+ - Add a server folder , and index file
+ - root location add .env
+
---
+++ server/index.js
@@ -0,0 +1,99 @@
+
+const needle = require('needle');
+const config = require('dotenv').config();
+const TOKEN = process.env.TWITTER_BEARER_TOKEN;
+
+const basURL = 'https://api.twitter.com';
+const rulesURL = basURL+'/2/tweets/search/stream/rules';
+const streamURL = basURL+'/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id';
+
+const rules = [
+    { value: 'giveaway' },
+     { value: 'xbox' }
+];
+
+// Get stream rules
+async function getRules() {
+    const response = await needle('get', rulesURL, {
+        headers: {
+            Authorization: `Bearer ${TOKEN}`
+        }
+    });
+    console.log(response.body);
+    return response.body;
+}
+// Set stream rules
+async function setRules() {
+    const data = {
+        add: rules,
+    }
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`,
+        }
+    });
+    return response.body;
+}
+// Delete stream rules
+async function deleteRules(rules) {
+    if( !Array.isArray(rules.data) ) {
+        return null;
+    }
+
+    // Array
+    const ids = rules.data.map((rule) => {
+        return rule.id;
+    });
+
+    const data = {
+        delete: {
+            ids: ids
+        }
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`
+        }
+    })
+    return response.body;
+}
+
+(async () => {
+    let currentRules;
+
+    try {
+        // Get all stream rules
+        currentRules = await getRules();
+
+        // Delete all stram rules
+        await deleteRules(currentRules);
+        
+        // Set rules base on array above 
+        await setRules();
+    } catch(error) {
+        console.error(error);
+        process.exit(1);
+    }
+
+    function streamTweats() {
+        const stream = needle.get(streamURL, {
+            headers: {
+                Authorization: `Bearer ${TOKEN}`
+            }
+            
+        })
+        stream.on('data', (data) => {
+            try {
+                const json = JSON.parse(data);
+                console.log(json);
+            } catch (error) {}
+        })
+    }
+
+    streamTweats();
+    
+})()
+
